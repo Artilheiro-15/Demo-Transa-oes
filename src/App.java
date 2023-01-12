67dc098aea6668eca56a46006d2407b882e51fcb
Atila jr <atilla.junior33@gmail.com>
2023-01-12 17:22:52 -0300

    Transaçoes

---
+++ src/App.java
@@ -0,0 +1,56 @@
+import db.DB;
+import db.DbException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("=====================================================");
+
+    Connection conn = null;
+    Statement st = null;
+    try {
+      conn = DB.getConnection();
+
+      conn.setAutoCommit(false);
+
+      st = conn.createStatement();
+
+      int rows1 = st.executeUpdate(
+        "UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1"
+      );
+
+      //    int x = 1;
+      //     if (x < 2) {
+      //      throw new SQLException("Fake error ");
+      //    }
+
+      int rows2 = st.executeUpdate(
+        "UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2"
+      );
+
+      conn.commit();
+
+      System.out.println("rows1 " + rows1);
+      System.out.println("rows2 " + rows2);
+    } catch (SQLException e) {
+      try {
+        conn.rollback();
+        throw new DbException(
+          "Transaction rolled back! Caused by: " + e.getMessage()
+        );
+      } catch (SQLException e1) {
+        throw new DbException(
+          "Error trying to rollback! Caused by: " + e1.getMessage()
+        );
+      }
+    } finally {
+      DB.closeStatement(st);
+      DB.closeConnection();
+    }
+
+    System.out.println("=====================================================");
+  }
+}
